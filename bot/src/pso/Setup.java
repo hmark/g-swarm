82729c82879e9bf6d09659fba3098c21a92c473f
hlavko <hlavino@gmail.com>
2012-10-21 13:33:10 +0200

    changed setup for PSO

--- bot/src/pso/Setup.java
+++ bot/src/pso/Setup.java
@@ -13,12 +13,12 @@ public class Setup {
 	/**
 	 * Number of particles in swarm.
 	 */
-	public static final int PARTICLES = 200;
+	public static final int PARTICLES = 10000;
 	
 	/**
 	 * Dimensions number of search space.
 	 */
-	public static final int DIMENSIONS = 15;
+	public static final int DIMENSIONS = 50;
 	
 	// PSO initial location and velocity coefficients
 	
@@ -30,34 +30,34 @@ public class Setup {
 	/**
 	 * Upper constraint of dimension value.
 	 */
-	public static final int XMAX = 100; 
+	public static final int XMAX = 255; 
 	
 	/**
-	 * Maximum speed of particle for dimension.
+	 * Maximum speed of particle for dimension (e.g. 255 means velocity is from interval <-255, 255>).
 	 */
-	public static final int SPEED_MAX = 100;
+	public static final int SPEED_MAX = 255;
 	
 	// PSO velocity update coefficients
 	
 	/**
 	 * Accelerator coefficient of cognitive component.
 	 */
-	public static final double C1 = 0.25;
+	public static final double C1 = 1.0;
 	
 	/**
 	 * Accelerator coefficient of social component.
 	 */
-	public static final double C2 = 0.75; 
+	public static final double C2 = 1.0; 
 	
 	/**
 	 * Minimum inertia weight coefficient.
 	 */
-	public static final double WMIN = 0.5;
+	public static final double WMIN = 0.4;
 	
 	/**
 	 * Starting inertia weight coefficient.
 	 */
-	public static final double WMAX = 1;
+	public static final double WMAX = 0.9;
 	
 	// PSO stop condition limits
 	
