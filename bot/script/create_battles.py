214880e907fe07a88923ab73f79de03bd76eb411
Marek Hlavac <mark.hlavac@gmail.com>
2013-03-04 14:22:12 +0100

    changed robot lists and method of evaluation

--- bot/script/create_battles.py
+++ bot/script/create_battles.py
@@ -5,26 +5,28 @@ ENEMIES_PATH = "C:/Users/h/Workspace/_github/g-swarm/bot/conf/robots.lst"
 TEMPLATE_PATH = "C:/Users/h/Workspace/_github/g-swarm/bot/conf/battle/gswarm.battle"
 TARGET_DIR = "C:/Robocode/battles/gswarm/"
 
-def createBattleFile(enemy, level, id):
-	battle_name = "gswarm" + id + "_" + level + ".battle"
-	robot_name = "gswarm.GSwarmRobot" + id
-	target_path = TARGET_DIR + battle_name
-	shutil.copyfile(TEMPLATE_PATH, target_path + "_")
 
-	# replace #ROBOT# string in target battle config
-	with open(target_path, "wt") as out:
-		for line in open(target_path + "_"):
-			out.write(line.replace('#ENEMY#', enemy).replace('#ROBOT#', robot_name))
-			
-	os.remove(target_path + "_")
+def createBattleFile(enemy, level, id):
+    battle_name = "gswarm" + id + "_" + level + ".battle"
+    robot_name = "gswarm.GSwarmRobot" + id
+    target_path = TARGET_DIR + battle_name
+    shutil.copyfile(TEMPLATE_PATH, target_path + "_")
+
+    # replace #ROBOT# string in target battle config
+    with open(target_path, "wt") as out:
+        for line in open(target_path + "_"):
+            out.write(line.replace('#ENEMY#', enemy).replace('#ROBOT#', robot_name))
+
+    os.remove(target_path + "_")
 
 with open(ENEMIES_PATH) as f:
     enemies = f.readlines()
 
-enemies = [enemy.split(" ")[1] for enemy in enemies]
+enemies = [enemy.split(" ")[1].replace("-", " ") for enemy in enemies]
+
 level = 0
 
 for enemy in enemies:
-	level += 1
-	for i in range(0, 99):
-		createBattleFile(enemy, str(level), str(10000 + i))
+    level += 1
+    for i in range(0, 99):
+        createBattleFile(enemy, str(level), str(10000 + i))
