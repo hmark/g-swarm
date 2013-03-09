ab050bf97c3dbc8aef377b68084f84f2cd5f0874
Marek Hlavac <mark.hlavac@gmail.com>
2013-03-09 11:01:44 +0100

    updated generating of multi fitness graphs

--- bot/script/Charts.py
+++ bot/script/Charts.py
@@ -3,68 +3,69 @@ import matplotlib.pyplot as plt
 __author__ = "Marek Hlavac (hmark.eu)"
 __license__ = "GPL"
 
+
 class PyChart:
-	"""Class for creating MatPlotLib charts.
-	"""
+    """Class for creating MatPlotLib charts.
+    """
 
-	def __init__(self, figure, title, xlabel, ylabel):
-		"""Constructor.
+    def __init__(self, figure, title, xlabel, ylabel):
+        """Constructor.
 
-	    Args:
-	        figure: chart number
-	        title: chart title
-	        xlabel: x-axis label
-	        ylabel: y-axis label
-	    """
-		fig = plt.figure(figure)
-		ax = fig.add_subplot(111)
-		ax.grid(True)
-		plt.title(title)
-		plt.xlabel(xlabel)
-		plt.ylabel(ylabel)
+        Args:
+            figure: chart number
+            title: chart title
+            xlabel: x-axis label
+            ylabel: y-axis label
+        """
+        fig = plt.figure(figure)
+        ax = fig.add_subplot(111)
+        ax.grid(True)
+        plt.title(title)
+        plt.xlabel(xlabel)
+        plt.ylabel(ylabel)
 
-	def addYLimit(self, lowerlimit, upperlimit):
-		"""Setting y-axis limit.
+    def addYLimit(self, lowerlimit, upperlimit):
+        """Setting y-axis limit.
 
-	    Args:
-	        lowerlimit: lower limit (usually zero)
-	        upperlimit: upper limit
-	    """
-		plt.ylim((lowerlimit, upperlimit))
+        Args:
+            lowerlimit: lower limit (usually zero)
+            upperlimit: upper limit
+        """
+        plt.ylim((lowerlimit, upperlimit))
 
-	def addPlotY(self, figure, values, label):
-		"""Setting y-axis values without x-axis values.
+    def addPlotY(self, figure, values, label):
+        """Setting y-axis values without x-axis values.
 
-	    Args:
-	        figure: chart number
-	        values: plot data
-	        label: data title
-	    """
-		plt.figure(figure)
-		plt.plot(values, label=label)
+        Args:
+            figure: chart number
+            values: plot data
+            label: data title
+        """
+        plt.figure(figure)
+        plt.plot(values, label=label)
 
-	def addPlotXY(self, figure, valuesx, valuesy, label):
-		"""Setting y-axis values with x-axis values.
+    def addPlotXY(self, figure, valuesx, valuesy, label):
+        """Setting y-axis values with x-axis values.
 
-	    Args:
-	        figure: chart number
-	        valuesx: plot data
-	        valuesy: plot data
-	        label: data title
-	    """
-		plt.figure(figure)
-		plt.plot(valuesx, valuesy, label=label)
-		
-	def addLegend(self, figure):
-		"""Create legend for chart based on plots.
+        Args:
+            figure: chart number
+            valuesx: plot data
+            valuesy: plot data
+            label: data title
+        """
+        plt.figure(figure)
+        plt.plot(valuesx, valuesy, label=label)
 
-	    Args:
-	        figure: chart number
-	    """
-		plt.figure(figure)
-		plt.legend(bbox_to_anchor=(0.98, 0.98), loc=1, borderaxespad=0.)
+    def addLegend(self, figure):
+        """Create legend for chart based on plots.
 
-	def show(self):
-		"""Draw and show all plots in different windows.
-	    """
-		plt.show()
+        Args:
+            figure: chart number
+        """
+        plt.figure(figure)
+        plt.legend(loc=2, borderaxespad=0.)
+
+    def show(self):
+        """Draw and show all plots in different windows.
+        """
+        plt.show()
--- bot/script/create_graph.py
+++ bot/script/create_graph.py
@@ -1,30 +1,53 @@
 import Charts
 from os import listdir
-from os.path import isfile, join
 
 chart = Charts.PyChart(0, 'Vývoj fitnes', 'Iterácia', 'Fitnes')
 gbest = []
-lbest = []
+lbest_ahead = []
+lbest_gun = []
+lbest_turn = []
+best = 0
 
-for f in listdir("../test/test2012-11-14-19-19-24"):
-	try:
-		f = open("../test/test2012-11-14-19-19-24/" + f + "/logs/data.log")
-		lines = f.readlines()
-		gbest.append(int(float(lines[1].split()[-1])))
-		lbest.append(int(float(lines[3].split()[-1])))
+for fpath in listdir("../test/test2013-03-07-08-21-32"):
+    try:
+        fname = "../test/test2013-03-07-08-21-32/" + fpath + "/logs/ahead_data.log"
+        f = open(fname, 'r')
+        lines = f.readlines()
 
-		if len(gbest) == 150:
-			break
+        best = max(best, int(float(lines[1].split()[-1])))
+        lbest_ahead.append(int(float(lines[3].split()[-1])))
 
-	except IOError:
-		break
+        fname = "../test/test2013-03-07-08-21-32/" + fpath + "/logs/gun_data.log"
+        f = open(fname, 'r')
+        lines = f.readlines()
+
+        best = max(best, int(float(lines[1].split()[-1])))
+        lbest_gun.append(int(float(lines[3].split()[-1])))
+
+        fname = "../test/test2013-03-07-08-21-32/" + fpath + "/logs/turn_data.log"
+        f = open(fname, 'r')
+        lines = f.readlines()
+
+        best = max(best, int(float(lines[1].split()[-1])))
+        lbest_turn.append(int(float(lines[3].split()[-1])))
+
+        gbest.append(best)
+
+        if len(gbest) == 100:
+            break
+
+    except IOError:
+        print(f)
+        break
 
 # drawing chart plots
 #chart.addYLimit(0, 15)
-print(len(gbest), len(lbest))
-chart.addYLimit(0, 40000)
-chart.addPlotXY(0, range(1, 151), gbest, "gBest")
-chart.addPlotXY(0, range(1, 151), lbest, "priemerná hodnota lBest")
+print(len(gbest), len(lbest_ahead), len(lbest_gun), len(lbest_turn))
+chart.addYLimit(0, 150)
+chart.addPlotXY(0, range(1, 101), gbest, "gBest")
+chart.addPlotXY(0, range(1, 101), lbest_ahead, "lBest modulu posunu")
+chart.addPlotXY(0, range(1, 101), lbest_gun, "lBest modulu natocenia hlavne")
+chart.addPlotXY(0, range(1, 101), lbest_turn, "lBest modulu natocenia tela")
 
 # add chart legend
 chart.addLegend(0)
