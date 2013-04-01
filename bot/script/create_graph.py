dc13c31b47fef1b27b3dcaa10a6d28b4241144b3
Marek Hlavac <mark.hlavac@gmail.com>
2013-04-01 11:20:09 +0200

    updated graph script

--- bot/script/create_graph.py
+++ bot/script/create_graph.py
@@ -8,28 +8,23 @@ lbest_gun = []
 lbest_turn = []
 best = 0
 
-for fpath in listdir("../test/test2013-03-07-08-21-32"):
+TARGET_DIR = "../test/test2013-03-27-23-52-50"
+
+for fpath in listdir(TARGET_DIR):
     try:
-        fname = "../test/test2013-03-07-08-21-32/" + fpath + "/logs/ahead_data.log"
+        fname = TARGET_DIR + "/" + fpath + "/logs/gun_data.log"
         f = open(fname, 'r')
         lines = f.readlines()
 
-        best = max(best, int(float(lines[1].split()[-1])))
-        lbest_ahead.append(int(float(lines[3].split()[-1])))
+        best = max(best, float(lines[1].split()[-1]))
+        lbest_gun.append(float(lines[3].split()[-1]))
 
-        fname = "../test/test2013-03-07-08-21-32/" + fpath + "/logs/gun_data.log"
+        fname = TARGET_DIR + "/" + fpath + "/logs/turn_data.log"
         f = open(fname, 'r')
         lines = f.readlines()
 
-        best = max(best, int(float(lines[1].split()[-1])))
-        lbest_gun.append(int(float(lines[3].split()[-1])))
-
-        fname = "../test/test2013-03-07-08-21-32/" + fpath + "/logs/turn_data.log"
-        f = open(fname, 'r')
-        lines = f.readlines()
-
-        best = max(best, int(float(lines[1].split()[-1])))
-        lbest_turn.append(int(float(lines[3].split()[-1])))
+        best = max(best, float(lines[1].split()[-1]))
+        lbest_turn.append(float(lines[3].split()[-1]))
 
         gbest.append(best)
 
@@ -43,9 +38,9 @@ for fpath in listdir("../test/test2013-03-07-08-21-32"):
 # drawing chart plots
 #chart.addYLimit(0, 15)
 print(len(gbest), len(lbest_ahead), len(lbest_gun), len(lbest_turn))
-chart.addYLimit(0, 150)
+chart.addYLimit(0, 3)
 chart.addPlotXY(0, range(1, 101), gbest, "gBest")
-chart.addPlotXY(0, range(1, 101), lbest_ahead, "lBest modulu posunu")
+#chart.addPlotXY(0, range(1, 101), lbest_ahead, "lBest modulu posunu")
 chart.addPlotXY(0, range(1, 101), lbest_gun, "lBest modulu natocenia hlavne")
 chart.addPlotXY(0, range(1, 101), lbest_turn, "lBest modulu natocenia tela")
 
